2f872b1ede915e3a53b2411911c9e872b9d7f61a
Atul Dubal <atuldubal199@gmail.com>
2025-02-11 00:52:00 +0530

    Initial Commit

---
+++ app/src/main/java/apcoders/in/carpark/OnBoardingActivityOne.java
@@ -0,0 +1,36 @@
+package apcoders.in.carpark;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OnBoardingActivityOne extends AppCompatActivity {
+    TextView btnNextTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_on_boarding_one);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnNextTextView = findViewById(R.id.btnNextTextView);
+
+        btnNextTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OnBoardingActivityOne.this, OnBoardingActivityTwo.class));
+            }
+        });
+    }
+}
