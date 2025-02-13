34fd9d1994a92e5b47038bd6843f44ba21e27386
Atul Dubal <atuldubal199@gmail.com>
2025-02-14 00:45:31 +0530

    Settings Design & WishListed

--- app/src/main/java/apcoders/in/carpark/AddVehicleActivity.java
+++ app/src/main/java/apcoders/in/carpark/AddVehicleActivity.java
@@ -14,7 +14,7 @@ public class AddVehicleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_vehicle);
+        setContentView(R.layout.successfullpayment);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
